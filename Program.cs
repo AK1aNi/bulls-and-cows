066a78b85f810544ad3e31d5e5728015267786a2
Muhammad Abdullah Kiani <22MKiani@heckgrammar.co.uk>
2025-03-04 10:00:35 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace bulls_and_cows
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int nums = r.Next(1,10);
+        }
+    }
+}
